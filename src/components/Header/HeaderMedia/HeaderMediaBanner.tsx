a75ab0120937e082eb52d459d9eeca47e0ad3ea3
nagibinstudio <nagibin.ars@gmail.com>
2024-05-28 14:36:17 +0300

    feature/1300

--- src/components/Header/HeaderMedia/HeaderMediaBanner.tsx
+++ src/components/Header/HeaderMedia/HeaderMediaBanner.tsx
@@ -4,24 +4,24 @@ import { Link } from 'react-router-dom';
 import HeaderMediaMenuBannerConcierge from '../../../assets/images/header-media-menu-banner-concierge.jpg';
 
 const HeaderMediaBanner: React.FC = () => {
-    return (
-        <Link to="/vipservice" className="header-media-modal-menu-banner">
-            <div
-                className="header-media-modal-menu-banner-image"
-                style={{
-                    backgroundImage: `url("${HeaderMediaMenuBannerConcierge}")`,
-                }}
-            ></div>
+	return (
+		<Link to="/vipservice" className="header-media-modal-menu-banner">
+			<div
+				className="header-media-modal-menu-banner-image"
+				style={{
+					backgroundImage: `url("${HeaderMediaMenuBannerConcierge}")`,
+				}}
+			></div>
 
-            <div className="header-media-modal-menu-banner-text">
-                <h4 className="header-media-modal-menu-banner-text__title">Консьерж сервис</h4>
+			<div className="header-media-modal-menu-banner-text">
+				<h4 className="header-media-modal-menu-banner-text__title">Консьерж сервис</h4>
 
-                <p className="header-media-modal-menu-banner-text__subtitle">
-                    Мы создали удобный сервис для всех, кто хочет продать от 10 лотов
-                </p>
-            </div>
-        </Link>
-    );
+				<p className="header-media-modal-menu-banner-text__subtitle">
+					Мы создали удобный сервис для всех, кто хочет продать от 7 лотов
+				</p>
+			</div>
+		</Link>
+	);
 };
 
 export default HeaderMediaBanner;
--- src/components/Home/HomeSellPartners/HomeSellPartners.tsx
+++ src/components/Home/HomeSellPartners/HomeSellPartners.tsx
@@ -2,35 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const HomeSellPartners: React.FC = () => {
-    return (
-        <div className="home-sell-partners">
-            <div className="home-sell-partners-block hover-scale">
-                <h2 className="home-sell-partners-block__title">Обмен</h2>
+	return (
+		<div className="home-sell-partners">
+			<div className="home-sell-partners-block hover-scale">
+				<h2 className="home-sell-partners-block__title">Обмен</h2>
 
-                <p className="home-sell-partners-block__description">
-                    Оставьте заявку на обмен лота — мы оценим его и предложим депозит, соответствующий стоимости вашей
-                    вещи. Его можно будет потратить при покупке любого товара на THE CULTT.
-                </p>
+				<p className="home-sell-partners-block__description">
+					Оставьте заявку на обмен лота — мы оценим его и предложим депозит, соответствующий стоимости вашей
+					вещи. Его можно будет потратить при покупке любого товара на THE CULTT.
+				</p>
 
-                <Link to="/cabinet/sell?type=exchange" className="btn home-sell-partners-block__btn">
-                    Подробнее
-                </Link>
-            </div>
+				<Link to="/cabinet/sell?type=exchange" className="btn home-sell-partners-block__btn">
+					Подробнее
+				</Link>
+			</div>
 
-            <div className="home-sell-partners-block hover-scale">
-                <h2 className="home-sell-partners-block__title">VIP-сервис</h2>
+			<div className="home-sell-partners-block hover-scale">
+				<h2 className="home-sell-partners-block__title">VIP-сервис</h2>
 
-                <p className="home-sell-partners-block__description">
-                    Мы создали удобный сервис для всех, кто хочет продать от 10 лотов - это могут быть сумки, обувь,
-                    аксессуары. Это сэкономит время, силы и даже нервы.
-                </p>
+				<p className="home-sell-partners-block__description">
+					Мы создали удобный сервис для всех, кто хочет продать от 7 лотов - это могут быть сумки, обувь,
+					аксессуары. Это сэкономит время, силы и даже нервы.
+				</p>
 
-                <Link to="/vipservice" className="btn home-sell-partners-block__btn">
-                    Подробнее
-                </Link>
-            </div>
-        </div>
-    );
+				<Link to="/vipservice" className="btn home-sell-partners-block__btn">
+					Подробнее
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default HomeSellPartners;
