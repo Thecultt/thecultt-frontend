d724cccf4fe039153aa5159db4abf67a113803d5
nagibinstudio <nagibin.ars@gmail.com>
2024-06-14 13:16:17 +0300

    feature/1290: Fix comments

--- src/components/SellInfo/SellInfoMain.tsx
+++ src/components/SellInfo/SellInfoMain.tsx
@@ -7,7 +7,7 @@ import { MEDIA_SIZES } from 'src/constants/styles';
 
 import { Select } from 'src/components';
 
-import { conditions } from 'src/constants/catalog';
+import { Conditions } from 'src/types/product';
 
 import SellInfoMainImage from 'src/assets/images/sell-info/sell-info-main.jpg';
 import Logo from 'src/assets/images/logo.svg';
@@ -16,7 +16,7 @@ const models: {
     [key: string]: {
         title: string;
         image: string;
-        prices: Record<conditions, number>;
+        prices: Record<Conditions, number>;
     };
 } = {
     hermesKelly: {
@@ -43,7 +43,7 @@ const SellInfoMain: React.FC = () => {
     const isMobile = useMediaQuery(`(max-width: ${MEDIA_SIZES.tablet})`);
 
     const [currentModel, setCurrentModel] = React.useState('hermesKelly');
-    const [currentCondition, setCurrentCondition] = React.useState<conditions>('Новое');
+    const [currentCondition, setCurrentCondition] = React.useState<Conditions>('Новое');
 
     return (
         <div className="sell-info-main">
@@ -96,7 +96,7 @@ const SellInfoMain: React.FC = () => {
                             <Select
                                 label="Состояние"
                                 items={[{ title: 'Новое' }, { title: 'Отличное' }, { title: 'Хорошее' }]}
-                                onChange={(value) => setCurrentCondition(value.title as conditions)}
+                                onChange={(value) => setCurrentCondition(value.title as Conditions)}
                             />
                         </div>
                     </div>
