ffe12c09597ed7ca45fc39bc988e19ca09736357
nagibinstudio <nagibin.ars@gmail.com>
2024-06-14 10:41:14 +0300

    HotFix RegLog Error Message

--- src/redux/actions/check_email.ts
+++ src/redux/actions/check_email.ts
@@ -9,57 +9,61 @@ import { CheckEmailActionTypes, CheckEmailActions } from '../types/ICheckEmail';
 import { sendRecoveryPassword } from './recovery_password';
 
 export const sendCheckEmail =
-    (email: string, callback: (type: ReglogStateTypesNotLogin) => void) =>
-    async (dispatch: Dispatch<CheckEmailActions>) => {
-        dispatch({
-            type: CheckEmailActionTypes.SET_CHECK_EMAIL_EMAIL,
-            payload: email,
-        });
+	(email: string, callback: (type: ReglogStateTypesNotLogin) => void) =>
+		async (dispatch: Dispatch<CheckEmailActions>) => {
+			dispatch({
+				type: CheckEmailActionTypes.SET_CHECK_EMAIL_EMAIL,
+				payload: email,
+			});
 
-        dispatch({
-            type: CheckEmailActionTypes.SET_CHECK_EMAIL_IS_SEND,
-            payload: true,
-        });
+			dispatch({
+				type: CheckEmailActionTypes.SET_CHECK_EMAIL_IS_SEND,
+				payload: true,
+			});
 
-        sessionStorage.setItem('email', email);
+			sessionStorage.setItem('email', email);
 
-        let nextPopupType = ReglogStateTypesNotLogin.LOGIN;
+			let nextPopupType = ReglogStateTypesNotLogin.LOGIN;
 
-        await axios
-            .post(`${process.env.REACT_APP_API_DOMEN}/email_check/`, { email })
-            .then(() => {
-                if (checkWarningEmail(email)) {
-                    nextPopupType = ReglogStateTypesNotLogin.WARNING_BLOCKED_EMAIL_REGISTER;
-                } else {
-                    nextPopupType = ReglogStateTypesNotLogin.REGISTER;
-                }
+			const ym = window.ym || (window.ym = []);
 
-                dispatch({
-                    type: CheckEmailActionTypes.SET_CHECK_EMAIL_IS_SEND,
-                    payload: false,
-                });
-            })
-            .catch(({ response: { data } }) => {
-                if (data.login_first_time) {
-                    nextPopupType = ReglogStateTypesNotLogin.OLD_USER_NEW_PASSWORD;
+			ym(68184745, 'reachGoal', 'login_email')
 
-                    dispatch(sendRecoveryPassword(email) as any);
-                } else if (checkWarningEmail(email)) {
-                    nextPopupType = ReglogStateTypesNotLogin.WARNING_BLOCKED_EMAIL_LOGIN;
-                } else {
-                    nextPopupType = ReglogStateTypesNotLogin.LOGIN;
-                }
+			await axios
+				.post(`${process.env.REACT_APP_API_DOMEN}/email_check/`, { email })
+				.then(() => {
+					if (checkWarningEmail(email)) {
+						nextPopupType = ReglogStateTypesNotLogin.WARNING_BLOCKED_EMAIL_REGISTER;
+					} else {
+						nextPopupType = ReglogStateTypesNotLogin.REGISTER;
+					}
 
-                dispatch({
-                    type: CheckEmailActionTypes.SET_CHECK_EMAIL_IS_SEND,
-                    payload: false,
-                });
+					dispatch({
+						type: CheckEmailActionTypes.SET_CHECK_EMAIL_IS_SEND,
+						payload: false,
+					});
+				})
+				.catch(({ response: { data } }) => {
+					if (data.login_first_time) {
+						nextPopupType = ReglogStateTypesNotLogin.OLD_USER_NEW_PASSWORD;
 
-                dispatch({
-                    type: CheckEmailActionTypes.SET_CHECK_EMAIL_IS_EXISTING,
-                    payload: true,
-                });
-            });
+						dispatch(sendRecoveryPassword(email) as any);
+					} else if (checkWarningEmail(email)) {
+						nextPopupType = ReglogStateTypesNotLogin.WARNING_BLOCKED_EMAIL_LOGIN;
+					} else {
+						nextPopupType = ReglogStateTypesNotLogin.LOGIN;
+					}
 
-        callback(nextPopupType);
-    };
+					dispatch({
+						type: CheckEmailActionTypes.SET_CHECK_EMAIL_IS_SEND,
+						payload: false,
+					});
+
+					dispatch({
+						type: CheckEmailActionTypes.SET_CHECK_EMAIL_IS_EXISTING,
+						payload: true,
+					});
+				});
+
+			callback(nextPopupType);
+		};
--- src/redux/actions/login.ts
+++ src/redux/actions/login.ts
@@ -5,124 +5,126 @@ import { Dispatch } from 'react';
 import { LoginActions, LoginActionTypes } from '../types/ILogin';
 
 export const sendLogin = (data: { username: string | null; password: string }, onChangeSuccess?: () => void) => {
-    return async (dispatch: Dispatch<LoginActions>) => {
-        dispatch({
-            type: LoginActionTypes.SET_LOGIN_IS_SEND,
-            payload: true,
-        });
+	return async (dispatch: Dispatch<LoginActions>) => {
+		dispatch({
+			type: LoginActionTypes.SET_LOGIN_IS_SEND,
+			payload: true,
+		});
 
-        return axios
-            .post(`${process.env.REACT_APP_API_DOMEN}/login/`, data)
-            .then(({ data }) => {
-                localStorage.setItem('accessToken', data.access);
-                localStorage.setItem('accessToken_is_remove', 'true');
+		return axios
+			.post(`${process.env.REACT_APP_API_DOMEN}/login/`, data)
+			.then(({ data }) => {
+				localStorage.setItem('accessToken', data.access);
+				localStorage.setItem('accessToken_is_remove', 'true');
 
-                window.dataLayer.push({ ecommerce: null }); // Clear the previous ecommerce object.
-                window.dataLayer.push({
-                    event: 'login',
-                    ecommerce: {
-                        timestamp: Math.floor(Date.now() / 1000),
-                    },
-                });
+				window.dataLayer.push({ ecommerce: null }); // Clear the previous ecommerce object.
+				window.dataLayer.push({
+					event: 'login',
+					ecommerce: {
+						timestamp: Math.floor(Date.now() / 1000),
+					},
+				});
 
-                try {
-                    if (localStorage.getItem('mindboxDeviceUUID')) {
-                        axios.post(
-                            `https://api.mindbox.ru/v3/operations/async?endpointId=thecultt.Website&operation=Website.AuthorizeCustomer&deviceUUID=${localStorage.getItem('mindboxDeviceUUID')}`,
-                            {
-                                // "customerAction": {
-                                // 	"customFields": {
-                                // 		"brand": "<Бренд>",
-                                // 		"coctoyanie": "<Состояние>",
-                                // 		"defecti": "<Дефекты>",
-                                // 		"kategoria": "<Категория>",
-                                // 		"model": "<Модель>",
-                                // 		"ojidaniePoTcene": "<Ожидание по цене>",
-                                // 		"otpravilAnketyNaProdazy": "<Отправил анкету на продажу>",
-                                // 		"photo1": "<Фото 1>",
-                                // 		"photo2": "<Фото 2>",
-                                // 		"photo3": "<Фото 3>",
-                                // 		"photo4": "<Фото 4>",
-                                // 		"tovarIzKulta": "<Товар приобретен в Культе>",
-                                // 		"visitNaProdat": "<Визит на страницу Продать>"
-                                // 	}
-                                // },
-                                customer: {
-                                    ids: {
-                                        websiteID: `${data.id}`,
-                                    },
-                                    // "discountCard": {
-                                    // 	"ids": {
-                                    // 		"number": "<Номер дисконтной карты>"
-                                    // 	}
-                                    // },
-                                    // "birthDate": "<Дата рождения>",
-                                    // "sex": "<Пол>",
-                                    // "timeZone": "<Часовой пояс>",
-                                    // "lastName": "<Фамилия>",
-                                    // "firstName": "<Имя>",
-                                    // "middleName": "<Отчество>",
-                                    // "fullName": "<ФИО>",
-                                    // "area": {
-                                    // 	"ids": {
-                                    // 		"externalId": "<Внешний идентификатор зоны>"
-                                    // 	}
-                                    // },
-                                    email: `${localStorage.getItem('email')}`,
-                                    // "mobilePhone": "<Мобильный телефон>",
-                                    // "customFields": {
-                                    // 	"tipKlienta": "<Тип клиента>",
-                                    // 	"gorod": "<Город>",
-                                    // 	"istochnikPodpiski": "<Источник подписки>"
-                                    // },
-                                    // "subscriptions": [
-                                    // ]
-                                },
-                                executionDateTimeUtc: new Date(),
-                            },
-                            {
-                                headers: {
-                                    'Content-Type': 'application/json; charset=utf-8',
-                                    Accept: 'application/json',
-                                    Authorization: 'Mindbox secretKey="Lyv5BiL99IxxpHRgOFX0N875s6buFjii"',
-                                },
-                            },
-                        );
-                    }
-                } catch (e) {
-                    console.log(e);
-                }
+				const ym = window.ym || (window.ym = []);
 
-                if (localStorage.getItem('redirect_reglog')) {
-                    window.location.href = localStorage.getItem('redirect_reglog') as string;
-                } else {
-                    window.location.hash = '';
-                    window.location.reload();
-                }
+				ym(68184745, 'reachGoal', 'login_password')
 
-                // if (onChangeSuccess) {
-                // 	onChangeSuccess()
-                // } else {
-                // 	window.location.hash = ""
-                // 	window.location.reload()
-                // }
+				try {
+					if (localStorage.getItem('mindboxDeviceUUID')) {
+						axios.post(
+							`https://api.mindbox.ru/v3/operations/async?endpointId=thecultt.Website&operation=Website.AuthorizeCustomer&deviceUUID=${localStorage.getItem('mindboxDeviceUUID')}`,
+							{
+								// "customerAction": {
+								// 	"customFields": {
+								// 		"brand": "<Бренд>",
+								// 		"coctoyanie": "<Состояние>",
+								// 		"defecti": "<Дефекты>",
+								// 		"kategoria": "<Категория>",
+								// 		"model": "<Модель>",
+								// 		"ojidaniePoTcene": "<Ожидание по цене>",
+								// 		"otpravilAnketyNaProdazy": "<Отправил анкету на продажу>",
+								// 		"photo1": "<Фото 1>",
+								// 		"photo2": "<Фото 2>",
+								// 		"photo3": "<Фото 3>",
+								// 		"photo4": "<Фото 4>",
+								// 		"tovarIzKulta": "<Товар приобретен в Культе>",
+								// 		"visitNaProdat": "<Визит на страницу Продать>"
+								// 	}
+								// },
+								customer: {
+									ids: {
+										websiteID: `${data.id}`,
+									},
+									// "discountCard": {
+									// 	"ids": {
+									// 		"number": "<Номер дисконтной карты>"
+									// 	}
+									// },
+									// "birthDate": "<Дата рождения>",
+									// "sex": "<Пол>",
+									// "timeZone": "<Часовой пояс>",
+									// "lastName": "<Фамилия>",
+									// "firstName": "<Имя>",
+									// "middleName": "<Отчество>",
+									// "fullName": "<ФИО>",
+									// "area": {
+									// 	"ids": {
+									// 		"externalId": "<Внешний идентификатор зоны>"
+									// 	}
+									// },
+									email: `${localStorage.getItem('email')}`,
+									// "mobilePhone": "<Мобильный телефон>",
+									// "customFields": {
+									// 	"tipKlienta": "<Тип клиента>",
+									// 	"gorod": "<Город>",
+									// 	"istochnikPodpiski": "<Источник подписки>"
+									// },
+									// "subscriptions": [
+									// ]
+								},
+								executionDateTimeUtc: new Date(),
+							},
+							{
+								headers: {
+									'Content-Type': 'application/json; charset=utf-8',
+									Accept: 'application/json',
+									Authorization: 'Mindbox secretKey="Lyv5BiL99IxxpHRgOFX0N875s6buFjii"',
+								},
+							},
+						);
+					}
+				} catch (e) {
+					console.log(e);
+				}
 
-                dispatch({
-                    type: LoginActionTypes.SET_LOGIN_IS_SEND,
-                    payload: false,
-                });
-            })
-            .catch(({ response }) => {
-                dispatch({
-                    type: LoginActionTypes.SET_LOGIN_IS_SEND,
-                    payload: false,
-                });
+				if (localStorage.getItem('redirect_reglog')) {
+					window.location.href = localStorage.getItem('redirect_reglog') as string;
+				} else {
+					window.location.hash = '';
+					window.location.reload();
+				}
 
-                if (response) {
-                    throw new SubmissionError({
-                        password: 'Неверный пароль',
-                    });
-                }
-            });
-    };
+				// if (onChangeSuccess) {
+				// 	onChangeSuccess()
+				// } else {
+				// 	window.location.hash = ""
+				// 	window.location.reload()
+				// }
+
+				dispatch({
+					type: LoginActionTypes.SET_LOGIN_IS_SEND,
+					payload: false,
+				});
+			})
+			.catch(() => {
+				dispatch({
+					type: LoginActionTypes.SET_LOGIN_IS_SEND,
+					payload: false,
+				});
+
+				throw new SubmissionError({
+					password: 'Неверный пароль',
+				});
+			});
+	};
 };
