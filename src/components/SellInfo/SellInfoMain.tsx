f78a3e3b18b82e554db4571e759e25b00ebab2c1
nagibinstudio <nagibin.ars@gmail.com>
2024-06-14 14:08:34 +0300

    feature/1290: Added Products calc

--- src/components/SellInfo/SellInfoMain.tsx
+++ src/components/SellInfo/SellInfoMain.tsx
@@ -37,6 +37,70 @@ const models: {
             Хорошее: 120000,
         },
     },
+    pradaReNylon: {
+        title: 'Prada Re-Nylon',
+        image: 'https://storage.yandexcloud.net/prod-thecultt/4033/a4da8d9068ca97e498142245ec1a286762663949.jpg',
+        prices: {
+            Новое: 112500,
+            Отличное: 63000,
+            Хорошее: 49000,
+        },
+    },
+    hermesShoes: {
+        title: 'Hermes сандалии',
+        image: 'https://storage.yandexcloud.net/prod-thecultt/23919/3493ac5ff6dad9b77ef8f9f15ebb5bb4a259fed5.jpg',
+        prices: {
+            Новое: 82500,
+            Отличное: 78750,
+            Хорошее: 56000,
+        },
+    },
+    chanelShoes: {
+        title: 'Chanel лоферы',
+        image: 'https://storage.yandexcloud.net/prod-thecultt/74023/rik7N7MCWKZ3p4DZe4kaRcML1LdB7bHr0Ni5T3fO.jpg',
+        prices: {
+            Новое: 127500,
+            Отличное: 105000,
+            Хорошее: 63000,
+        },
+    },
+    louisVuittonWallet: {
+        title: 'Louis Vuitton Wallet',
+        image: 'https://storage.yandexcloud.net/prod-thecultt/5560/ff465c10d19e543dca4843518716e31828993819.jpg',
+        prices: {
+            Новое: 38500,
+            Отличное: 26000,
+            Хорошее: 18300,
+        },
+    },
+    gucciBelt: {
+        title: 'Gucci Belt',
+        image: 'https://storage.yandexcloud.net/prod-thecultt/4327/48e29de16d51817d3c286e74cd4302ddcb35d52f.jpg',
+        prices: {
+            Новое: 26000,
+            Отличное: 22750,
+            Хорошее: 18300,
+        },
+    },
+    celineKnotBracelet: {
+        title: 'Celine Knot Bracelet',
+        image: 'https://storage.yandexcloud.net/prod-thecultt/73491/T30E4vpsJvvSAuHYlIu2u2ZBBvKqNIHA8vtkUmGn.jpg',
+        prices: {
+            Новое: 32200,
+            Отличное: 26000,
+            Хорошее: 18300,
+        },
+    },
+
+    cartierPanthere: {
+        title: 'Cartier Panthère',
+        image: 'https://storage.yandexcloud.net/prod-thecultt/73092/cJcOuOdLUuCdw6GaWsHcXKn2HXOMTwQ0cx2ofVd2.jpg',
+        prices: {
+            Новое: 440000,
+            Отличное: 345000,
+            Хорошее: 300000,
+        },
+    },
 };
 
 const SellInfoMain: React.FC = () => {
