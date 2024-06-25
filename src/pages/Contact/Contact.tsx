f70a47293288f47999bec265546ed90c69424ba2
nagibinstudio <nagibin.ars@gmail.com>
2024-06-25 11:52:07 +0300

    Changed time

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -242,7 +242,7 @@ const Contact: React.FC = () => {
                         <div className="contact-block-office hover-scale">
                             <h3 className="contact-block-office__title">Адрес офиса</h3>
                             <p className="contact-block-office__description">
-                                Мы находимся по адресу Трубная 25с3 (3 этаж, офис THE CULTT) ежедневно с 11:00 до 21:00.
+                                Мы находимся по адресу Трубная 25с3 (3 этаж, офис THE CULTT) ежедневно с 10:00 до 21:00.
                                 Для посещения необходима регистрация!
                             </p>
                             <a
