4878db9acf07d5bf11a0b7333fd85ce1b7101caa
Ilya <lekomtzev.ilja@gmail.com>
2024-07-09 16:16:17 +0300

    feature/1599: updated visit info

--- src/components/Home/HomeVisit/HomeVisit.tsx
+++ src/components/Home/HomeVisit/HomeVisit.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { VISIT_AVAILABLE } from 'src/constants/app';
+
 import VisitImage from 'src/assets/images/home/home-visit.jpg';
 import VisitImageMedia from 'src/assets/images/home/home-visit-media.jpg';
+
+const title = VISIT_AVAILABLE ? 'Забронируйте свой визит в THE CULTT!' : 'Визит в THE CULTT';
+const description = VISIT_AVAILABLE ? (
+    'Примерьте сумки и аксессуары лучших дизайнеров из коллекции THE CULTT или встретьтесь с нашим экспертом, чтобы узнать, сколько вы сможете заработать на своих украшениях.'
+) : (
+    <>
+        Внимание! Клиентская зона закрыта с 10.07. Это временно — мы обновляем пространство. Как только запись в
+        клиентскую зону откроется снова, мы пришлем зарегистрированным пользователям уведомление.
+        <br />
+        <br />
+        Пока мы обновляемся, остальные опции работают как обычно. Закажите доставку и примерку понравившихся лотов на
+        дом или оформите заявку на продажу своих аксессуаров.
+    </>
+);
 
 const HomeVisit: React.FC = () => {
     return (
         <>
             <div className="home-visit hover-scale">
                 <div className="home-visit-text">
-                    <h2 className="home-visit-text__title">Забронируйте свой визит в THE CULTT!</h2>
+                    <h2 className="home-visit-text__title">{title}</h2>
+                    <p className="home-visit-text__description">{description}</p>
 
-                    <p className="home-visit-text__description">
-                        Примерьте сумки и аксессуары лучших дизайнеров из коллекции THE CULTT или встретьтесь с нашим
-                        экспертом, чтобы узнать, сколько вы сможете заработать на своих украшениях.
-                    </p>
-
-                    <Link to="/visit" className="btn home-visit-text__btn">
-                        Перейти
-                    </Link>
+                    {VISIT_AVAILABLE && (
+                        <Link to="/visit" className="btn home-visit-text__btn">
+                            Перейти
+                        </Link>
+                    )}
                 </div>
 
                 <div
@@ -31,16 +45,14 @@ const HomeVisit: React.FC = () => {
 
             <div className="home-visit-media" style={{ backgroundImage: `url('${VisitImageMedia}')` }}>
                 <div className="home-visit-media-text">
-                    <h2 className="home-visit-media-text__title">Забронируйте свой визит в THE CULTT!</h2>
+                    <h2 className="home-visit-media-text__title">{title}</h2>
+                    <p className="home-visit-media-text__description">{description}</p>
 
-                    <p className="home-visit-media-text__description">
-                        Примерьте сумки и аксессуары лучших дизайнеров из коллекции THE CULTT или встретьтесь с нашим
-                        экспертом, чтобы узнать, сколько вы сможете заработать на своих украшениях.
-                    </p>
-
-                    <Link to="/visit" className="btn-regular white home-visit-media-text__btn">
-                        Перейти
-                    </Link>
+                    {VISIT_AVAILABLE && (
+                        <Link to="/visit" className="btn-regular white home-visit-media-text__btn">
+                            Перейти
+                        </Link>
+                    )}
                 </div>
             </div>
         </>
